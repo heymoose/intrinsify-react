f958bba858c8f44f29b84443f4b365601cdd6d5a
heymoose <marcusanovak@gmail.com>
2018-12-28 13:57:53 -0700

    Finish separating dummy data logic from component

--- src/components/IntrinsifyTable/IntrinsifyTable.js
+++ src/components/IntrinsifyTable/IntrinsifyTable.js
@@ -1,47 +1,13 @@
 import React, { Component } from 'react';
-import { retrieveStockData } from './intrinsifyTableLogic';
+import { generateDummyStockData, generateColumns } from './intrinsifyTableLogic';
 import ReactTable from 'react-table';
 import style from 'react-table/react-table.css'
 
-// TODO: Can add column generation to retrieveStockData.js and make more generic as well
-
 class IntrinsifyTable extends Component {
     render () {
-        const data = retrieveStockData(new Array(15).fill(null));
-        const columns = [
-            {
-                Header: 'Name',
-                accessor: 'name'
-            },
-            {
-                Header: 'Ticker',
-                accessor: 'ticker'
-            },
-            {
-                Header: 'Price',
-                accessor: 'price'
-            },
-            {
-                Header: 'Flat Estimate Growth',
-                accessor: 'flatEstimateGrowth'
-            },
-            {
-                Header: 'AAA Corporate Bond Yield',
-                accessor: 'aaaCorpBondYield'
-            },
-            {
-                Header: 'EPS',
-                accessor: 'eps'
-            },
-            {
-                Header: 'IV',
-                accessor: 'iv'
-            },
-            {
-                Header: 'NIV',
-                accessor: 'niv'
-            }
-        ]
+        const data = generateDummyStockData(15);
+        const columns = generateColumns();
+
         return (
             <ReactTable
                 className={style.ReactTable}
--- src/components/IntrinsifyTable/intrinsifyTableLogic.js
+++ src/components/IntrinsifyTable/intrinsifyTableLogic.js
@@ -1,37 +1,62 @@
 
+const dummyStockNames = {
+    'mmm': '3M Co',
+    'ge': 'General Electric Company',
+    'ko': 'The Coca Cola Co',
+    'hsy': 'Hershey Co',
+    'mcd': 'McDonalds Corp',
+    'pep': 'PepsiCo, Inc.',
+    'dis': 'Walt Disney Co',
+    'de': 'Deere & Company'
+};
+
+const columnsAccessorToName = {
+    'name': 'Name',
+    'ticker': 'Ticker',
+    'price': 'Price',
+    'flatGrowthEstimate': 'Flat Growth Estimate',
+    'aaaCorpBondYield': 'AAA Corporate Bond Yield',
+    'eps': 'EPS',
+    'iv': 'IV',
+    'riv': 'RIV',
+};
+
 const randNumInRange = (max) => {
     return max * Math.random() << 0;
 }
 
-const createDummyData = (tickers) => {
-    const stockTickerToName = {
-        'mmm': '3M Co',
-        'ge': 'General Electric Company',
-        'ko': 'The Coca Cola Co',
-        'hsy': 'Hershey Co',
-        'mcd': 'McDonalds Corp',
-        'pep': 'PepsiCo, Inc.',
-        'dis': 'Walt Disney Co',
-        'de': 'Deere & Company'
-    };
-    const tickerKeys = Object.keys(stockTickerToName);
+const createDummyData = (num) => {
+    const tickerKeys = Object.keys(dummyStockNames);
     let randKey;
     
-    return tickers.map(tick => {
-        randKey = tickerKeys[randNumInRange(tickerKeys.length)];
-        return {
-            "name": stockTickerToName[randKey],
-            "ticker": randKey.toUpperCase(),
-            "price": randNumInRange(100),
-            "flatEstimateGrowth": 5,
-            "aaaCorpBondYield": 3.56,
-            "eps": randNumInRange(10),
-            "iv": Math.random().toFixed(2),
-            "niv": Math.random().toFixed(2)
-        }
-    });
+    return new Array(num)
+        .fill(null)
+        .map(tick => {
+            randKey = tickerKeys[randNumInRange(tickerKeys.length)];
+
+            return {
+                "name": dummyStockNames[randKey],
+                "ticker": randKey.toUpperCase(),
+                "price": randNumInRange(100),
+                "flatGrowthEstimate": 5,
+                "aaaCorpBondYield": 3.56,
+                "eps": randNumInRange(10),
+                "iv": Math.random().toFixed(2),
+                "riv": Math.random().toFixed(2)
+            }
+        });
 }
 
-export const retrieveStockData = (tickers) => {
-    return createDummyData(tickers);
+export const generateDummyStockData = (numDummyData) => {
+    return createDummyData(numDummyData);
+}
+
+export const generateColumns = () => {
+    return Object.keys(columnsAccessorToName)
+        .map(acc => {
+            return { 
+                Header: columnsAccessorToName[acc],
+                accessor: acc
+             };
+        });
 }
