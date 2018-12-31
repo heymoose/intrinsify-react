d3714584a469f14a0711fc9da1bf8a189337b645
heymoose <marcusanovak@gmail.com>
2018-12-31 11:37:39 -0500

    Add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Header.module.css';
+
+const header = () => (
+    <h1 className={style.Header}>Intrinsify</h1>
+);
+
+export default header;
--- src/containers/IntrinsifyContainer/IntrinsifyContainer.js
+++ src/containers/IntrinsifyContainer/IntrinsifyContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
 import IntrinsifyTable from '../../components/IntrinsifyTable/IntrinsifyTable';
 
 class IntrinsifyContainer extends Component {
     render() {
         return (
             <>
+                <Header />
                 <p>The text input box to input tickers</p>
                 <p>The submit button</p>
                 <IntrinsifyTable />
