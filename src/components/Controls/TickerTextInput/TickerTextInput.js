2fb23fbdfc4f7dac4ef880d3cd9cf697237ffdc8
heymoose <marcusanovak@gmail.com>
2019-01-04 12:43:34 -0500

    Use material for input text box

--- src/components/Controls/TickerTextInput/TickerTextInput.js
+++ src/components/Controls/TickerTextInput/TickerTextInput.js
@@ -1,15 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import style from './TickerTextInput.module.css';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-class TickerTextInput extends Component {
-    render() {
-        return (
-            <div>
-                <input className={style.TextInput} type='text' />
-            </div>
-        );
-    }
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class TickerTextInput extends React.Component {
+  state = {
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR',
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-full-width"
+          label="Tickers"
+          style={{ margin: 8 }}
+          placeholder="KO, DIS, GE"
+          helperText="Enter tickers to calculate intrinsic value."
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
+    );
+  }
 }
 
-export default TickerTextInput;
+TickerTextInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TickerTextInput);
