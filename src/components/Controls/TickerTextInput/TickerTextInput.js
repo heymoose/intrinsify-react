c64e8c17e4d0c0472e042b95bcabc9e8f4ea3fcd
heymoose <marcusanovak@gmail.com>
2019-01-06 16:13:23 -0700

    Simplify text input

--- src/components/Controls/TickerTextInput/TickerTextInput.js
+++ src/components/Controls/TickerTextInput/TickerTextInput.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
@@ -20,45 +19,21 @@ const styles = theme => ({
   },
 });
 
-class TickerTextInput extends React.Component {
-  state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  };
-
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <form className={classes.container} noValidate autoComplete="off">
+const tickerTextInput = () => (
+    <form>
         <TextField
-          id="outlined-full-width"
-          label="Tickers"
-          style={{ margin: 8 }}
-          placeholder="KO, DIS, GE"
-          helperText="Enter tickers to calculate intrinsic value."
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </form>
-    );
-  }
-}
+            id="outlined-full-width"
+            label="Tickers"
+            style={{ margin: 8 }}
+            placeholder="KO, DIS, GE"
+            helperText="Enter tickers to calculate intrinsic value."
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+                shrink: true,
+            }} />
+    </form>
+);
 
-TickerTextInput.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(TickerTextInput);
+export default withStyles(styles)(tickerTextInput);
