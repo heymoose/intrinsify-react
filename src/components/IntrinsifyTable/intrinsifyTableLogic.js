42705b639420c8fcf9fd0c700320a1079f913bad
heymoose <marcusanovak@gmail.com>
2019-01-07 10:54:45 -0700

    Polish and finish mapping of riv indicator color and width

--- src/components/IntrinsifyTable/intrinsifyTableLogic.js
+++ src/components/IntrinsifyTable/intrinsifyTableLogic.js
@@ -44,6 +44,40 @@ export const rgbToHex = (r, g, b) => {
     return ((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
 }
 
+// Source for color mapping code:
+// https://stackoverflow.com/questions/16360533/calculate-color-hex-having-2-colors-and-percent-position
+
+const getComponentFromHexColorCode = (color, component) => {
+    if (component === 'r') {
+        return color.substring(0, 2);
+    }
+    else if (component === 'g') {
+        return color.substring(2, 4);
+    }
+    else if (component === 'b') {
+        return color.substring(4, 6);
+    }
+    
+    return color;
+}
+
+const findColorComponentRatioBetweenTwoColors = (comp1, comp2, ratio) => {
+    return Math.ceil(parseInt(comp1, 16) * ratio + parseInt(comp2, 16) * (1 - ratio));
+}
+
+const findColorRatioBetweenTwoColors = (color1, color2, ratio) => {
+    const rComponentColor1 = getComponentFromHexColorCode(color1, 'r');
+    const rComponentColor2 = getComponentFromHexColorCode(color2, 'r');
+    const gComponentColor1 = getComponentFromHexColorCode(color1, 'g');
+    const gComponentColor2 = getComponentFromHexColorCode(color2, 'g');
+    const bComponentColor1 = getComponentFromHexColorCode(color1, 'b');
+    const bComponentColor2 = getComponentFromHexColorCode(color2, 'b');
+    const r = findColorComponentRatioBetweenTwoColors(rComponentColor1, rComponentColor2, ratio);
+    const g = findColorComponentRatioBetweenTwoColors(gComponentColor1, gComponentColor2, ratio);
+    const b = findColorComponentRatioBetweenTwoColors(bComponentColor1, bComponentColor2, ratio);
+    return rgbToHex(r, g, b);
+}
+
 const createDummyData = (num) => {
     const tickerKeys = Object.keys(dummyStockNames);
     let randKey;
@@ -55,7 +89,7 @@ const createDummyData = (num) => {
             const price = randFloatInRange(5, 1000, 2);
             const eps = randFloatInRange(1, 10, 2);
             const iv = (eps * (8.5 + (2 * flatGrowthEstimate)) * 4.4) / aaaCorpBondYield;
-            const riv = randFloatInRange(0, 3, 2);
+            const riv = randFloatInRange(0, 2, 2);
 
             return {
                 'name': dummyStockNames[randKey],
@@ -86,31 +120,36 @@ export const generateColumns = () => {
 }
 
 export const mapIndicatorValue = (value) => {
-    // This is finally working, but a better solution for sliding the color from light to dark
-    // red or green is probably this:
-    // https://stackoverflow.com/questions/16360533/calculate-color-hex-having-2-colors-and-percent-position
-
+    const greenLowerBound = 'ccffe5';
+    const greenUpperBound = '00994c';
+    const redLowerBound = 'ffcccc';
+    const redUpperBound = '990000';
     const constrainedValue = Math.min(Math.max(parseFloat(value), 0), 3); // lock value between 0 and 3
-    let r = 0;
-    let g = 0;
-    let b = 0;
+    let percentBetweenMinAndMax = 0;
+    let hex = 'd3d3d3';
 
     if (constrainedValue >= 0 && constrainedValue <= 1) {
-        r = 255;
-        g = Math.abs(scale(constrainedValue, 0, 3, 0, 204) - 204);
-        b = g;
+        percentBetweenMinAndMax = constrainedValue;
+        hex = findColorRatioBetweenTwoColors(redLowerBound, redUpperBound, percentBetweenMinAndMax);
     }
-    else if (constrainedValue > 1 && constrainedValue <= 3) {
-        r = Math.abs(scale(constrainedValue, 0, 3, 0, 192) - 192);
-        g = 255;
-        b = Math.abs(scale(constrainedValue, 0, 3, 51, 209) - 158);
+    else if (constrainedValue > 1 && constrainedValue <= 3.0) {
+        percentBetweenMinAndMax = constrainedValue / 3.0;
+        hex = findColorRatioBetweenTwoColors(greenUpperBound, greenLowerBound, percentBetweenMinAndMax);
     }
-    
-    return '#' + rgbToHex(parseInt(r), parseInt(g), parseInt(b));
+
+    return '#' + hex;
 }
 
 export const mapIndicatorWidth = (value) => {
     const constrainedValue = Math.min(Math.max(parseFloat(value), 0), 3); // lock value between 0 and 3
-    const width = scale(constrainedValue, 0, 3, 25, 100);
+    let width = 25;
+
+    if (constrainedValue >= 0 && constrainedValue <= 1) {
+        width = scale(Math.abs(constrainedValue - 1), 0, 1, 15, 100);
+    }
+    else if (constrainedValue > 1 && constrainedValue <= 3.0) {
+        width = scale(constrainedValue, 1, 3, 15, 100);
+    }
+    
     return width + '%';
 }
