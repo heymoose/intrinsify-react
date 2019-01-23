f533f4852751c9597c14c2e510d6f96be54bbc7e
heymoose <marcusanovak@gmail.com>
2019-01-23 12:39:16 -0700

    Add test for saving configs

--- src/store/reducers/configs.js
+++ src/store/reducers/configs.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../jsUtils/immutableUpdate';
+import cuid from 'cuid';
 
 export const initialState = {
     savedConfigs: [],
@@ -15,10 +16,24 @@ export const setCurrentIntrinsifyConfig = (state, action) => {
     });
 };
 
+export const saveIntrinsifyConfig = (state, action) => {
+    const config = updateObject(action.config, {
+        id: cuid()
+    });
+
+    const updatedSavedConfigs = state.savedConfigs.concat(config);
+
+    return updateObject(state, {
+        savedConfigs: updatedSavedConfigs
+    });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SET_CURRENT_INTRINSIFY_CONFIG:
             return setCurrentIntrinsifyConfig(state, action);
+        case actionTypes.SAVE_INTRINSIFY_CONFIG:
+            return saveIntrinsifyConfig(state, action);
         default:
             return state;
     }
--- src/store/reducers/configs.test.js
+++ src/store/reducers/configs.test.js
@@ -1,5 +1,6 @@
 import reducer, { initialState, setCurrentIntrinsifyConfig } from './configs';
 import * as actionTypes from '../actions/actionTypes';
+import cuid from 'cuid';
 
 describe('configs reducer', () => {
     it('should return the initial state', () => {
@@ -8,17 +9,27 @@ describe('configs reducer', () => {
 
     it('should set the current intrinsify config', () => {
         const config = {
-            type: actionTypes.SET_CURRENT_INTRINSIFY_CONFIG,
-            config: { id: '', tickers: ['KO', 'DIS'] }
-        };
-        const expectedOutput = {
-            ...initialState,
-            currentConfig: {
-                id: '',
-                tickers: ['KO', 'DIS']
-            }
-        };
+                type: actionTypes.SET_CURRENT_INTRINSIFY_CONFIG,
+                config: { id: '', tickers: ['KO', 'DIS'] }
+            },
+            expectedOutput = {
+                ...initialState,
+                currentConfig: {
+                    id: '',
+                    tickers: ['KO', 'DIS']
+                }
+            };
 
         expect(reducer(initialState, config)).toEqual(expectedOutput);
+    });
+
+    it('should add config to the list of saved configs', () => {
+        const config = {
+                type: actionTypes.SAVE_INTRINSIFY_CONFIG,
+                config: { tickers: ['KO', 'DIS'] }
+            },
+            output = reducer(initialState, config);
+
+        expect(output.savedConfigs).toHaveLength(1);
     });
 });
